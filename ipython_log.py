8068225f2cc1ce603e0e6ec4936ac597a8367748
orenovadia <orenovad@gmail.com>
2016-03-30 19:38:26 +0200

    learning curves

---
+++ ipython_log.py
@@ -0,0 +1,119 @@
+# IPython log file
+
+import sklearn
+import sklearn.learning_curve
+get_ipython().magic(u'logstart')
+get_ipython().magic(u'ls ')
+get_ipython().magic(u'ls -ltr')
+import sklearn
+import sklearn.learning_curve
+import sklearn.datasets
+data = sklearn.datasets.load_iris()
+dir(data)
+data.train
+data['DATA']
+data['train']
+dir(data)
+data.data
+data.values
+help(data)
+help(sklearn.datasets.load_iris)
+data.target
+data.data
+len(data.data)
+len(data.target)
+get_ipython().magic(u'cat ipython_log.py')
+get_ipython().magic(u'startlog')
+get_ipython().magic(u'logstart')
+get_ipython().magic(u'logstop')
+get_ipython().magic(u'logstart')
+get_ipython().magic(u'ls ')
+import sklearn
+import sklearn.learning_curve
+import sklearn.datasets
+data = sklearn.datasets.load_iris()
+from sklearn.naive_bayes import GaussianNB
+X_train, X_test, y_train, y_test =  sklearn.cross_validation.train_test_split(
+            data.data,data.target,)
+help(GaussianNB)
+clf = GaussianNB()
+clf.fit(X_train,y_train)
+clf
+print clf
+clf.predict([1,2,3,4])
+_
+__[0]
+__[1]
+clf.predict(X_test)
+y_test
+import sklearn.cross_validation
+sklearn.cross_validation
+get_ipython().magic(u'pinfo sklearn.cross_validation')
+get_ipython().magic(u'pinfo sklearn.cross_validation.check_scoring')
+get_ipython().magic(u'pinfo sklearn.cross_validation.check_cv')
+import sklearn.svm
+get_ipython().magic(u'pinfo sklearn.svm.SVC')
+get_ipython().magic(u'pinfo sklearn.svm.SVC')
+get_ipython().magic(u'pinfo sklearn.svm.SVC')
+get_ipython().magic(u'pinfo sklearn.svm.SVC')
+help(sklearn.svm.SVC)
+get_ipython().set_next_input(u'help(sklearn.svm.SVC');get_ipython().magic(u'pinfo sklearn.svm.SVC')
+get_ipython().magic(u'pinfo sklearn.svm.SVC')
+help(sklearn.svm.SVC)
+get_ipython().magic(u'pinfo sklearn.svm.SVC')
+X_test.max
+X_test.max()
+X_test.min()
+from  sklearn.learning_curve import  learning_curve
+learning_curve>
+get_ipython().magic(u'pinfo learning_curve')
+get_ipython().magic(u'pinfo learning_curve')
+get_ipython().magic(u'pinfo learning_curve')
+estimator = GaussianNB()
+learning_curve(estimator,data.data,data,target)
+learning_curve(estimator,data.data,data.target)
+estimator.fit(X_train,y_train)
+estimator.fit(X_train,y_train)
+estimator.fit(X_train,y_train)
+estimator.fit(X_train,y_train)
+estimator.fit(X_train,y_train)
+estimator.fit(X_train,y_train)
+estimator.fit(X_train,y_train)
+estimator.fit(X_train,y_train)
+learning_curve(estimator,data.data,data.target)
+get_ipython().magic(u'pinfo _')
+get_ipython().magic(u'pinfo learning_curve')
+estimator.name
+estimator.__name__
+print estimator
+estimator = GaussianNB
+learning_curve(estimator,data.data,data.target)
+learning_curve(estimator(),data.data,data.target)
+learning_curve(estimator(),data.data,data.target,train_sizes=np.linspace(0.1,0.5,5))
+learning_curve(estimator,data.data,data.target,train_sizes=np.linspace(0.1,0.5,5))
+learning_curve(estimator,data.data,data.target,train_sizes=np.linspace(0.1,0.5,5))
+get_ipython().magic(u'pinfo learning_curve')
+from sklearn.linear_model.logistic import LogisticRegression
+get_ipython().magic(u'pinfo LogisticRegression')
+t = LogisticRegression()
+t.fit
+t.predict
+get_ipython().magic(u'pinfo GaussianNB')
+get_ipython().magic(u'pinfo LogisticRegression')
+from sklearn.linear_model import LogisticRegressionCV
+get_ipython().magic(u'pinfo LogisticRegressionCV')
+get_ipython().magic(u'pinfo LogisticRegressionCV')
+get_ipython().magic(u'pinfo GaussianNB')
+get_ipython().magic(u'pinfo GaussianNB')
+get_ipython().magic(u'pinfo GaussianNB.__init__')
+learning_curve(LogisticRegression,data.data,data.target,train_sizes=np.linspace(0.1,0.5,5))
+learning_curve(LogisticRegression(),data.data,data.target,train_sizes=np.linspace(0.1,0.5,5))
+learning_curve(GaussianNB(),data.data,data.target,train_sizes=np.linspace(0.1,0.5,5))
+from sklearn.neural_network.rbm import BernoulliRBM
+get_ipython().magic(u'pinfo BernoulliRBM')
+from sklearn.neural_network import BernoulliRBM
+get_ipython().magic(u'pinfo BernoulliRBM')
+get_ipython().set_next_input(u'from sklearn.linear_model.Perceptron');get_ipython().magic(u'pinfo sklearn.linear_model.Perceptron')
+get_ipython().magic(u'pinfo sklearn.linear_model.Perceptron')
+from sklearn.tree import DecisionTreeClassifier
+get_ipython().magic(u'pinfo DecisionTreeClassifier')
---
+++ learning_curve.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import sklearn.cross_validation
+import sklearn.cross_validation
+import sklearn.datasets
+import sklearn.svm
+from sklearn.learning_curve import learning_curve
+from sklearn.linear_model.logistic import LogisticRegressionCV
+from sklearn.linear_model import Perceptron
+from sklearn.metrics import mean_squared_error
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+
+data = sklearn.datasets.load_iris()
+
+
+def learn_c(data, estimator, train_sizes=np.linspace(0.5, 0.9, 5)):
+    for train_size in []:#train_sizes:
+        X_train, X_test, y_train, y_test = sklearn.cross_validation.train_test_split(
+            data.data, data.target,
+            train_size=train_size)
+        clf = estimator()
+        clf.fit(X_train, y_train)
+        y_pred = clf.predict(X_test)
+        mse = mean_squared_error(y_test, y_pred)
+        print '''\
+        Train size  : %s
+        MSE         :%s
+        Estimator   : %s''' % (train_size, mse, 'est')
+
+        print '-' * 30
+
+    train_sizes, train_scores, test_scores = learning_curve(
+        estimator(), data.data, data.target, train_sizes=train_sizes, n_jobs=1
+    )
+    plt.figure()
+    plt.title(str(estimator))
+    plt.xlabel("Training examples")
+    plt.ylabel("Score")
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    train_scores_std = np.std(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+    test_scores_std = np.std(test_scores, axis=1)
+    plt.fill_between(train_sizes, train_scores_mean - train_scores_std,
+                     train_scores_mean + train_scores_std, alpha=0.1,
+                     color="r")
+    plt.fill_between(train_sizes, test_scores_mean - test_scores_std,
+                     test_scores_mean + test_scores_std, alpha=0.1, color="g")
+    plt.plot(train_sizes, train_scores_mean, 'o-', color="r",
+             label="Training score")
+    plt.plot(train_sizes, test_scores_mean, 'o-', color="g",
+             label="Cross-validation score")
+    plt.grid()
+    plt.legend(loc='best')
+
+
+for e in GaussianNB, LogisticRegressionCV,Perceptron,DecisionTreeClassifier:
+    learn_c(data, e)
+plt.show()
